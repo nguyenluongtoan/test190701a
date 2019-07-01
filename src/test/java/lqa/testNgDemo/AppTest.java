dfa6115f82ea77e9f24e16c74e6cdc6443c380d2
nguyenluongtoan <toanmta1990@gmail.com>
2019-07-01 09:16:02 +0700

    first commit

---
+++ src/test/java/lqa/testNgDemo/AppTest.java
@@ -0,0 +1,41 @@
+package lqa.testNgDemo;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+//    extends TestCase
+{
+//    /**
+//     * Create the test case
+//     *
+//     * @param testName name of the test case
+//     */
+//    public AppTest( String testName )
+//    {
+//        super( testName );
+//    }
+//
+//    /**
+//     * @return the suite of tests being tested
+//     */
+//    public static Test suite()
+//    {
+//        return new TestSuite( AppTest.class );
+//    }
+//
+//    /**
+//     * Rigourous Test :-)
+//     */
+//    public void testApp()
+//    {
+//        assertTrue( true );
+//    }
+	@Test
+	public void verifyTrue() {
+		Assert.assertTrue(true);
+	}
+}
