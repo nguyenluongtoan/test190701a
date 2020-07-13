b688be250c53dd20cdebe45b07b7ac26cf725c37
nguyenluongtoan <toanmta1990@gmail.com>
2020-07-13 09:29:22 +0700

    commit cua toi

--- src/main/java/lqa/testNgDemo/App.java
+++ src/main/java/lqa/testNgDemo/App.java
@@ -11,6 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello Mars!" );
         System.out.println( "Hello Moon!" );
-        
+        System.out.println( "Hello Neptune!" );
     }
 }
